8293e5ba7ccafa854b62fa6023628d8fc0f7152c
Tao119 <tao.dama.art@gmail.com>
2024-04-09 17:46:11 +0900

    fix

--- Formula/addpath.rb
+++ Formula/addpath.rb
@@ -1,7 +1,7 @@
 class Addpath < Formula
   desc "cli path utility written in Rust"
   homepage "https://github.com/Tao119/addpath"
-  url "https://github.com/Tao119/addpath/releases/tag/release#:~:text=addpath%2D0.1.0%2Dx86_64%2Dapple%2Ddarwin.tar.gz"
+  url "https://github.com/Tao119/addpath/releases/download/release/addpath-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "3137bb6d0592ce0347fd7faade463cc1527a04612405055e43497da02458f541"
   version "0.1.0"
 

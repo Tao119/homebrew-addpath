7a4071c537e0d4294ba9e8f4b68686a7f87ec10d
Tao119 <tao.dama.art@gmail.com>
2024-04-09 17:49:45 +0900

    0.1.1

--- Formula/addpath.rb
+++ Formula/addpath.rb
@@ -3,7 +3,7 @@ class Addpath < Formula
   homepage "https://github.com/Tao119/addpath"
   url "https://github.com/Tao119/addpath/releases/download/release/addpath-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "3137bb6d0592ce0347fd7faade463cc1527a04612405055e43497da02458f541"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "addpath"
